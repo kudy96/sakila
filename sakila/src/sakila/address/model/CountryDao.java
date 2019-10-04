88afc951685ac52818ac6c77b8d0dd717de3ea39
Win10 <Win10@DESKTOP-MQTS750>
2019-10-04 17:37:38 +0900

    sakila 데이터베이스 우선 큰 비중을 차지하는 address, business, customer, inventory 4가지로 구분을 짓고 패키지를 나누었습니다. 기존과 동일하게 컨트롤러 패키지에는 서블릿을 모델 패키지에는 Dao와 각 테이블 객체를 만들었습니다. 인덱스를 만들고 css를 적용하여 그 인덱스를 다른 html파일에도 동일 하게 적용시켰음 country.html에 전체 테이블 출력과 입력이 가능하게 만듬 목록은 항상 나오고 입력 또는 이전,다음 버튼 클릭시 페이지 이동이없고 데이터 이동을 통해 보여주게 하였음 페이지 다음버튼에서 비동기식으로 인해 코드가 순서대로 진행되지않아 asynk false를 통해 동기식으로 바꾸어 넘어가게 함 이로인해 데이터 이동시 끊김현상이 보임

---
+++ sakila/src/sakila/address/model/CountryDao.java
@@ -0,0 +1,88 @@
+package sakila.address.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import sakila.db.DBHelper;
+public class CountryDao {
+	
+	public int selectCountry() {
+		int count = 0;
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		String sql = "select count(*) from country";
+		try {
+			conn = DBHelper.getConnection();
+			stmt = conn.prepareStatement(sql);
+			rs = stmt.executeQuery();
+			
+			if(rs.next()) {
+				count = rs.getInt("count(*)");
+				
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			DBHelper.close(rs, stmt, conn);
+		}
+		return count;
+	}
+	
+	//selectList
+	public List<Country> selectCountryList(int currentPage){
+		List<Country> list = new ArrayList<Country>();
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		int rowPerPage = 10;
+		int beginRow = (currentPage-1) * rowPerPage;
+		
+		String sql = "select * from country order by country_id desc limit ?,?";
+		try {
+			conn = DBHelper.getConnection();
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, beginRow);
+			stmt.setInt(2, rowPerPage);
+			rs = stmt.executeQuery();
+			
+			while(rs.next()) {
+				Country c = new Country();
+				c.setCountryId(rs.getInt("country_id"));
+				c.setCountry(rs.getString("country"));
+				c.setLastUpdate(rs.getString("last_update"));
+				list.add(c);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBHelper.close(rs, stmt, conn);			
+		}
+		return list;
+	}
+	
+	
+	//insert
+	public void insertCountry(Country country) {
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		String sql = "insert into country(country, last_update) values(?,now())";
+		
+		try {
+			conn = DBHelper.getConnection();
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, country.getCountry());
+			stmt.executeUpdate();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBHelper.close(null, stmt, conn);			
+		}
+	}
+}
